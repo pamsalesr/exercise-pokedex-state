2990609268349358d6bd9f04ab5ade0b9b960c2c
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-03-01 17:55:57 -0300

    Add day 12.1 exercise

---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Button({handleClick}) {
+  return (
+    <button onClick={handleClick}>Next</button>
+  );
+};
+
+export default Button;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import Pokemon from './Pokemon';
+import Button from './Button';
+import data from './data';
 
 class Pokedex extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleClick = this.nextPokemon.bind(this)
+        this.state = {
+            pokemonIndex: 0,
+        }
+    }
+/*
+    handleClick(data) {
+        this.setState((state) => ({ pokemonIndex: (state.pokemonIndex + 1) % data.length, }))
+    };
+
     render() {
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon key={data.id} pokemon={data[this.state.pokemonIndex]} />
+                <Button onClick= { () => this.handleClick(data)}>Next</Button>
             </div>
         );
     }
+*/
+    nextPokemon(data) {
+    this.setState(state => ({
+      pokemonIndex: (state.pokemonIndex + 1) % data.length,
+    }));
+  }
+
+    render() {
+        return (
+            <div className="pokedex">
+                <Pokemon key={data.id} pokemon={data[this.state.pokemonIndex]} />
+                <Button
+                    className="pokedex-button"
+                    onClick={() => this.nextPokemon(data.length)}
+                    disabled={ data.length <= 1 }
+                > Próximo pokémon
+                </Button>
+            </div>
+        )
+    }
+
 }
 
 export default Pokedex;
